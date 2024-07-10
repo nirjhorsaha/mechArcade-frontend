f58fb01d6a2d64527e67e3ab300965166b038e99
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-07-10 14:40:08 +0600

    🚑️ Initial redux setup

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,14 +1,14 @@
-// // Need to use the React-specific entry point to import createApi
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-// // Define a service using a base URL and expected endpoints
-// export const baseApi = createApi({
-//   reducerPath: 'baseApi',
-//   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/' }),
-//   endpoints: () => ({
-//   }),
-// })
+// Define a service using a base URL and expected endpoints
+export const baseApi = createApi({
+  reducerPath: 'baseApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/' }),
+  endpoints: () => ({
+  }),
+})
 
-// // Export hooks for usage in functional components, which are
-// // auto-generated based on the defined endpoints
-// // export const { } = baseApi
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+// export const { } = baseApi
