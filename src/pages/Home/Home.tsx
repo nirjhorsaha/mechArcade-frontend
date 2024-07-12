14a2ac77e4edd2419a7e65a20e30203f861c1c80
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-07-12 16:17:23 +0600

    [Added] components in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,9 @@
+import Additional from "@/components/Additinonal/Additional";
+import CustomerReview from "@/components/Customer-Review/CustomerReview";
+import FeaturedBrand from "@/components/Featured-Brand/FeaturedBrand";
+import FeaturedProduct from "@/components/Featured-Product/FeaturedProduct";
 import Hero from "@/components/Hero/Hero";
+import Service from "@/components/Service/Service";
 import { Helmet } from "react-helmet";
 
 const Home = () => {
@@ -7,7 +12,12 @@ const Home = () => {
       <Helmet>
         <title>Home - Mech Arcade</title>
       </Helmet>
-      <Hero/>
+      <Hero />
+      <Service />
+      <FeaturedProduct />
+      <FeaturedBrand />
+      <CustomerReview />
+      <Additional/>
     </div>
   );
 };
