8a86c580b633141b288d6e96d99f83f40c8285cf
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-07-26 18:31:56 +0600

    [Added] cart item and price in cart button

--- src/pages/Shared/Navbar.tsx
+++ src/pages/Shared/Navbar.tsx
@@ -1,7 +1,20 @@
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo.png';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
 
-const Navbar = () => {
+
+const Navbar: React.FC = () => {
+  const cartItems = useSelector((state: RootState) => state.cart.items);
+  
+  // Calculate the total item count
+  const cartItemCount = cartItems.length;
+  
+  // Calculate the subtotal
+  const cartSubtotal = cartItems.reduce((total, item) => total + item.price, 0);
+  console.log(cartItems,cartItemCount, cartSubtotal)
+
   return (
     <div className="navbar bg-base-100 mb-5">
       <div className="navbar-start">
@@ -156,7 +169,7 @@ const Navbar = () => {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <span className="badge badge-sm indicator-item">8</span>
+              <span className="badge badge-sm indicator-item">{cartItemCount}</span>
             </div>
           </div>
           <div
@@ -164,10 +177,10 @@ const Navbar = () => {
             className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
           >
             <div className="card-body">
-              <span className="text-lg font-bold">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
+              <span className="text-lg font-bold">{cartItemCount} Items</span>
+              <span className="text-info">Subtotal: ${cartSubtotal.toFixed(2)}</span>
               <div className="card-actions">
-              <NavLink to="/cart" className="btn btn-primary btn-block">
+                <NavLink to="/cart" className="btn btn-primary btn-block">
                   View Cart
                 </NavLink>
               </div>
